6655d6b666908b7cf1af6fc1fadcc453e956ef6c
Josh Matos <Josh@MacBook-Pro-2.local>
2016-02-27 07:49:21 -0600

    looking good

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@
 var leftColumn = document.querySelector('#leftColumn')
 var rightColumn = document.querySelector('#rightColumn')
 var repoDiv = document.querySelector('#repoDiv')
-
+var inputEl = document.querySelector('input')
 var baseURL = 'https://api.github.com/users/'
 var myURL = baseURL + 'jmatos87'
+
+inputEl.placeholder = 'Touch and Enter Me'
+
 
 
 var myPromise = $.getJSON(myURL)
@@ -22,12 +25,12 @@ var showData = function (JSONdata) {
 //Left Column Francis Pull
 var objectToHTML = function (jsonObj){
 	//console.log(jsonObj)
-	var HTMLstring = '<div id="pictureContainer"><img src="' +jsonObj.avatar_url + '"></div>'
+	var HTMLstring = '<img src="' +jsonObj.avatar_url + '">'
 	HTMLstring += '<div id=leftTextContainer><p id="name" class="leftText">'+ jsonObj.name + '</p>'
-	HTMLstring += '<p class="leftText">User Name: '+ jsonObj.login + '</p>'
-	HTMLstring += '<p class="leftText">Location: '+ jsonObj.location + '</p>'
-	HTMLstring += '<p class="leftText">E-mail: '+ jsonObj.email + '</p>'
-	HTMLstring += '<p class="leftText">Job: Unemployed but has a big heart and great personality</p></div>'
+	HTMLstring += '<p class="leftText"><i class="material-icons">directions_run</i>'+ jsonObj.login + '</p>'
+	HTMLstring += '<p class="leftText"><i class="material-icons">location_searching</i>'+ jsonObj.location + '</p>'
+	HTMLstring += '<p class="leftText"><i class="material-icons">email</i>'+ jsonObj.email + '</p>'
+	HTMLstring += '<p class="leftText" id="work"><i class="material-icons">work</i>Unemployed but has a big heart and great personality</p></div>'
 
 	
 	leftColumn.innerHTML = HTMLstring
@@ -40,13 +43,52 @@ var	arrayToHTML = function (jsonArr){
 	var ArrString= ''
  for(var i = 0; i < jsonArr.length; i++){
  	var currentObj = jsonArr[i]
- 	console.log(currentObj)
- 	ArrString += '<div class="tableRow">'+ currentObj.name + '</div>'
+ 	// console.log(currentObj)
+ 	ArrString += '<div class="tableRow"><div class="tableName"><a href src='+ currentObj.url + '>' + currentObj.name + '</a></div>'
+ 	if (currentObj.description!== null){ArrString += '<div class="tableDescription"><p>' + currentObj.description + '</p></div>'}
+ 	ArrString += '</div>'
  }	
 repoDiv.innerHTML = ArrString 	
 
 
 }
+
+
+
+//Search Stuff
+
+
+var inputToUrl = function(keyEvent) {
+	var inputEl = keyEvent.target
+	if (keyEvent.keyCode === 13) {		
+		var userName = inputEl.value
+		inputEl.value = ''
+		location.hash = userName
+	}
+}
+
+
+
+var doSearchRequest = function (userName) {
+	var profileUrl = baseURL + userName
+	var userNamePromise = $.getJSON(profileUrl)
+	userNamePromise.then(objectToHTML)//for the left column
+	var reposUrl = baseURL + userName + '/repos'
+	var userReposPromise = $.getJSON(reposUrl)
+	userReposPromise.then(arrayToHTML)//for the right column
+}
+
+var controller = function() {
+	var userName = location.hash.substr(1)
+	doSearchRequest(userName) 
+}
+
+
+
+
+inputEl.addEventListener("keydown",inputToUrl)
+
+window.addEventListener("hashchange",controller)
 
 myPromise.then(objectToHTML)
 myWork.then(arrayToHTML)
